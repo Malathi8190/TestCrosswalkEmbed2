a51add2f74908051f0d0581a2da95f7d26805798
Malathi8190 <malu8190@gmail.com>
2016-11-29 17:48:11 +0530

    aa

--- app/src/main/java/com/zhsoft88/testcrosswalkembed2/MainActivity.java
+++ app/src/main/java/com/zhsoft88/testcrosswalkembed2/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mXWalkView = (XWalkView) findViewById(R.id.activity_main);
+        mXWalkView = (XWalkView) findViewById(R.id.xwalk);
         mXWalkView.load("http://zhuatang.com/", null);
     }
 
